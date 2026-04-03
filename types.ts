7f4a885cab3136ee826f70f5e039cbc40a2eb80a
fyrot <fyrot@outlook.com>
2026-04-02 21:35:52 -0500

    Initial setup

---
+++ types.ts
@@ -0,0 +1,10 @@
+export enum TimerCommand {
+
+}
+
+export enum TimerType {
+
+}
+
+// commands will be pause, resume, etc. so we can manage state
+// planned types for v1 will prob be countdown and clock
