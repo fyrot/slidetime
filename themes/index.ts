7f4581a5ccb1d177617ce7f3f7c384dc28548e8c
xedrft <matthewmatthewson4@outlook.com>
2026-04-14 22:01:38 -0500

    Added green theme (Moss).

--- themes/index.ts
+++ themes/index.ts
@@ -2,12 +2,14 @@ import type { PopupTheme } from "~popup/theme"
 import dark from "./dark.json"
 import aggie from "./aggie.json"
 import light from "./light.json"
+import green from "./green.json"
 
 // Add your themes here!!
 const allThemes: PopupTheme[] = [
   light as PopupTheme,
   dark as PopupTheme,
   aggie as PopupTheme,
+  green as PopupTheme,
 
 ]
 
